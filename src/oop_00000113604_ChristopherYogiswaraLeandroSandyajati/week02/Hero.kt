48530a04f16927fc99f788849e323574a9b3a05d
CYogiswara <112369336+CYogiswara@users.noreply.github.com>
2026-02-09 22:19:17 +0700

    week02: (task 2) creeate Hero class

---
+++ src/oop_00000113604_ChristopherYogiswaraLeandroSandyajati/week02/Hero.kt
@@ -0,0 +1,26 @@
+package oop_00000113604_ChristopherYogiswaraLeandroSandyajati.week02
+
+class Hero(
+    val name: String,
+    var hp: Int = 100,
+    val baseDamage: Int
+){
+    fun attack(targetName: String){
+        println("$name menebas ${targetName}!")
+    }
+
+    fun takeDamage(damage: Int): Int{
+       hp -= damage
+       if( hp < 0){
+           hp = 0
+       }
+       return hp
+    }
+
+    fun isAlive():Boolean{
+        if(hp > 0){
+            return true
+        }
+        return false
+    }
+}
