2cef769724ac5d4481afced81306169bab7fb172
CYogiswara <112369336+CYogiswara@users.noreply.github.com>
2026-02-10 15:41:07 +0700

    Fix 4

--- src/oop_00000113604_ChristopherYogiswaraLeandroSandyajati/week02/Main.kt
+++ src/oop_00000113604_ChristopherYogiswaraLeandroSandyajati/week02/Main.kt
@@ -1,46 +1,43 @@
-//package oop_00000113604_ChristopherYogiswaraLeandroSandyajati.week02
-//
-//import java.util.Scanner
-//
-//fun main(){
-//    val scanner = Scanner(System.`in`)
-//
-////    println("--- APLIKASI PMB UMN ---")
-////
-////    print("Masukan nama: ")
-////    val name = scanner.nextLine()
-////
-////    print("Masukan NIM (Wajib 5 karakter): ")
-////    val nim = scanner.next()
-////
-////    scanner.nextLine()
-////
-////    if(nim.length != 5){
-////        println("ERROR: Pendaftaran dibatalkan. NIM harus 5 karakter!")
-////    }else{
-////        print("Masukan jurusan: ")
-////        val major = scanner.nextLine()
-////
-////        val s1 = Student(name, nim, major)
-////        println("Status: pendaftaran selesai")
-////    }
-////
-////    print("Pilih Jalur (1. Reguler, 2. Umum): ")
-////    val type = scanner.nextInt()
-////    scanner.nextLine()
-////
-////    if(type == 1){
-////        print("Masukan jurusan: ")
-////        val major = scanner.nextLine()
-////        val s1 = Student(name, nim, major)
-////        println("Terdaftar di ${s1.major} dengan GPA awal ${s1.gpa}")
-////    }else if (type == 2){
-////        val s2 = Student(name, nim)
-////        println("Terdaftar di  ${s2.major} dengan GPA awal ${s2.gpa}")
-////    }else{
-////        println("Pilihan ngawur, pendaftaran batal!")
-////    }
-//
-////    library()
-//    game()
-//}
+package oop_00000113604_ChristopherYogiswaraLeandroSandyajati.week02
+
+import java.util.Scanner
+
+fun main(){
+    val scanner = Scanner(System.`in`)
+
+    println("--- APLIKASI PMB UMN ---")
+
+    print("Masukan nama: ")
+    val name = scanner.nextLine()
+
+    print("Masukan NIM (Wajib 5 karakter): ")
+    val nim = scanner.next()
+
+    scanner.nextLine()
+
+    if(nim.length != 5){
+        println("ERROR: Pendaftaran dibatalkan. NIM harus 5 karakter!")
+    }else{
+        print("Masukan jurusan: ")
+        val major = scanner.nextLine()
+
+        val s1 = Student(name, nim, major)
+        println("Status: pendaftaran selesai")
+    }
+
+    print("Pilih Jalur (1. Reguler, 2. Umum): ")
+    val type = scanner.nextInt()
+    scanner.nextLine()
+
+    if(type == 1){
+        print("Masukan jurusan: ")
+        val major = scanner.nextLine()
+        val s1 = Student(name, nim, major)
+        println("Terdaftar di ${s1.major} dengan GPA awal ${s1.gpa}")
+    }else if (type == 2){
+        val s2 = Student(name, nim)
+        println("Terdaftar di ${s2.major} dengan GPA awal ${s2.gpa}")
+    }else{
+        println("Pilihan ngawur, pendaftaran batal!")
+    }
+}
