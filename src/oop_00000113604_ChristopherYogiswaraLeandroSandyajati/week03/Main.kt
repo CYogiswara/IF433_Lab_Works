99495fbf9be44984dc18de93e696e23a26b33f86
CYogiswara <112369336+CYogiswara@users.noreply.github.com>
2026-02-19 22:11:04 +0700

    week03: cleanup code

--- src/oop_00000113604_ChristopherYogiswaraLeandroSandyajati/week03/Main.kt
+++ src/oop_00000113604_ChristopherYogiswaraLeandroSandyajati/week03/Main.kt
@@ -1,12 +1,12 @@
-package oop_00000113604_ChristopherYogiswaraLeandroSandyajati.week03
-
-fun main(){
-    val e = Employee("Budi")
-    e.salary = -1000
-    e.salary = 5000000
-    println("gajiL ${e.salary}")
-
-    e.increasePerformance()
-
-    println("Pajak yang harus dibayar: ${e.tax}")
-}
+//package oop_00000113604_ChristopherYogiswaraLeandroSandyajati.week03
+//
+//fun main(){
+//    val e = Employee("Budi")
+//    e.salary = -1000
+//    e.salary = 5000000
+//    println("gajiL ${e.salary}")
+//
+//    e.increasePerformance()
+//
+//    println("Pajak yang harus dibayar: ${e.tax}")
+//}
--- src/oop_00000113604_ChristopherYogiswaraLeandroSandyajati/week03/employee.kt
+++ src/oop_00000113604_ChristopherYogiswaraLeandroSandyajati/week03/employee.kt
@@ -1,30 +1,30 @@
-package oop_00000113604_ChristopherYogiswaraLeandroSandyajati.week03
-
-
-
-class Employee(val name: String){
-    var salary: Int = 0
-    set(value){
-        if(value < 0){
-            println("ERROR: Gaji tidak boleh negatif! diset ke 0")
-            field = 0
-        }else{
-            field = value
-        }
-    }
-
-    private var performanceRating: Int = 3
-
-    fun increasePerformance(){
-        performanceRating++
-        println("Kinerja $name meningkat! Rating: $performanceRating")
-    }
-
-    fun printStatus(){
-        println("Kinerja $name meningkat, Rating: $performanceRating")
-    }
-
-    val tax: Double
-        get() = salary * 0.1
-}
-
+//package oop_00000113604_ChristopherYogiswaraLeandroSandyajati.week03
+//
+//
+//
+//class Employee(val name: String){
+//    var salary: Int = 0
+//    set(value){
+//        if(value < 0){
+//            println("ERROR: Gaji tidak boleh negatif! diset ke 0")
+//            field = 0
+//        }else{
+//            field = value
+//        }
+//    }
+//
+//    private var performanceRating: Int = 3
+//
+//    fun increasePerformance(){
+//        performanceRating++
+//        println("Kinerja $name meningkat! Rating: $performanceRating")
+//    }
+//
+//    fun printStatus(){
+//        println("Kinerja $name meningkat, Rating: $performanceRating")
+//    }
+//
+//    val tax: Double
+//        get() = salary * 0.1
+//}
+//
