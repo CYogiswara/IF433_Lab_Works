420bc2ad4f6a86c93f1884f51b80cc8982a60797
CYogiswara <112369336+CYogiswara@users.noreply.github.com>
2026-02-19 22:04:04 +0700

    week03: add private property

--- src/oop_00000113604_ChristopherYogiswaraLeandroSandyajati/week03/employee.kt
+++ src/oop_00000113604_ChristopherYogiswaraLeandroSandyajati/week03/employee.kt
@@ -1,15 +1,6 @@
 package oop_00000113604_ChristopherYogiswaraLeandroSandyajati.week03
 
-private var performanceRating: Int = 3
 
-fun increasePerformance(){
-    performanceRating++
-    println("Kinerja $name meningkat! Rating: $performanceRating")
-}
-
-fun printStatus(){
-    println("Kinerja $name meningkat, Rating: $performanceRating")
-}
 
 class Employee(val name: String){
     var salary: Int = 0
@@ -21,4 +12,16 @@ class Employee(val name: String){
             field = value
         }
     }
+
+    private var performanceRating: Int = 3
+
+    fun increasePerformance(){
+        performanceRating++
+        println("Kinerja $name meningkat! Rating: $performanceRating")
+    }
+
+    fun printStatus(){
+        println("Kinerja $name meningkat, Rating: $performanceRating")
+    }
 }
+
