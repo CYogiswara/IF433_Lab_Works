544a9f28c0b50e46153b0b7d58fcc6f932a6f6d6
CYogiswara <112369336+CYogiswara@users.noreply.github.com>
2026-02-24 14:51:00 +0700

    week04: (task 1) test elecrtricCar in main

--- src/oop_00000113604_ChristopherYogiswaraLeandroSandyajati/week04/ElectricCar.kt
+++ src/oop_00000113604_ChristopherYogiswaraLeandroSandyajati/week04/ElectricCar.kt
@@ -1,2 +1,7 @@
 package oop_00000113604_ChristopherYogiswaraLeandroSandyajati.week04
 
+open class ElectricCar(brand: String, numberOfDoors: Int, val batteryCapacity: Int): car(brand, numberOfDoors){
+    final override fun accelerate(){
+        println("$brand berakselerasi dalam sunyi. Kapasitas baterai: $batteryCapacity%")
+    }
+}
--- src/oop_00000113604_ChristopherYogiswaraLeandroSandyajati/week04/main.kt
+++ src/oop_00000113604_ChristopherYogiswaraLeandroSandyajati/week04/main.kt
@@ -1,2 +1,20 @@
 package oop_00000113604_ChristopherYogiswaraLeandroSandyajati.week04
 
+fun main(){
+    println("--- Testing Vehicle ---")
+    val generalVehicle = Vehicle("Sepeda ontel")
+    generalVehicle.honk()
+    generalVehicle.accelerate()
+
+    println("\n--- Testing Car ---")
+    val myCar = car("Toyota", 4)
+    myCar.openTrunk()
+    myCar.honk()
+    myCar.accelerate()
+
+    println("\n--- Testing Electric Car ---")
+    val electricCar = ElectricCar("Tesla", 4, 100)
+    electricCar.accelerate()
+    electricCar.honk()
+    electricCar.openTrunk()
+}
