46bdcbd4222715e4c6f918dcaedccf5d4066dbd9
CYogiswara <112369336+CYogiswara@users.noreply.github.com>
2026-03-04 08:31:46 +0700

    week05: (task 2) add smart casting for EWallet topUp recovery

--- src/oop_00000113604_ChristopherYogiswaraLeandroSandyajati/week05/Main.kt
+++ src/oop_00000113604_ChristopherYogiswaraLeandroSandyajati/week05/Main.kt
@@ -4,9 +4,19 @@ fun main(){
     val eWalletObject = EWallet("yogi", 50.000)
     val creditCardObject = creditCard("yogi", 100.000)
 
-    val PaymentMethods: List<PaymentMethod> = listOf(eWalletObject, creditCardObject)
+    val Methods: List<PaymentMethod> = listOf(eWalletObject, creditCardObject)
 
-    for(PaymentMethod in PaymentMethods){
-        PaymentMethod.processPayment(75000.0)
+    for(Method in Methods){
+        Method.processPayment(75000.0)
+
+        when(Method){
+            is EWallet -> {
+                eWalletObject.topUp(50000.0)
+            }
+            is creditCard -> {
+                creditCardObject.processPayment((95000.0))
+            }
+        }
+
     }
 }
