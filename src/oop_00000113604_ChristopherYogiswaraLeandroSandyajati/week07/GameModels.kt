5aede6d9f3c5028ee5615585c44f5f7752b3dd72
CYogiswara <112369336+CYogiswara@users.noreply.github.com>
2026-03-17 16:18:40 +0700

    week07: (task) create GameItem data class

--- src/oop_00000113604_ChristopherYogiswaraLeandroSandyajati/week07/GameModels.kt
+++ src/oop_00000113604_ChristopherYogiswaraLeandroSandyajati/week07/GameModels.kt
@@ -1,9 +1,13 @@
 package oop_00000113604_ChristopherYogiswaraLeandroSandyajati.week07
 
-enum class GameModels(val dropChance: Int){
+enum class ItemRarity(val dropChance: Int){
     COMMON(70),
     UNCOMMON(60),
     RARE(50),
     EPIC(40),
     LEGENDARY(30)
+}
+
+data class GameItem(val name: String, val damage: Int, val rarity: ItemRarity){
+
 }
