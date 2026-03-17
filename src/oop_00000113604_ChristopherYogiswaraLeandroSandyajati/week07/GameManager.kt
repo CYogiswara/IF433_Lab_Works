2ab9aa4882f98ae13ab11e87d53d91ba106d64c9
CYogiswara <112369336+CYogiswara@users.noreply.github.com>
2026-03-17 16:12:00 +0700

    week07: (task) create ItemRarity enum

--- src/oop_00000113604_ChristopherYogiswaraLeandroSandyajati/week07/GameManager.kt
+++ src/oop_00000113604_ChristopherYogiswaraLeandroSandyajati/week07/GameManager.kt
@@ -1,2 +1,14 @@
 package oop_00000113604_ChristopherYogiswaraLeandroSandyajati.week07
 
+object GameManager{
+    var isGameRunning: Boolean = false
+
+    fun startGame(){
+        if(isGameRunning){
+            println("Game sudah berjalan! Mencegah instansiasi ganda")
+        }else{
+            isGameRunning = true
+            println("Memulai game engine...")
+        }
+    }
+}
---
+++ src/oop_00000113604_ChristopherYogiswaraLeandroSandyajati/week07/GameModels.kt
@@ -0,0 +1,9 @@
+package oop_00000113604_ChristopherYogiswaraLeandroSandyajati.week07
+
+enum class GameModels(val dropChance: Int){
+    COMMON(70),
+    UNCOMMON(60),
+    RARE(50),
+    EPIC(40),
+    LEGENDARY(30)
+}
