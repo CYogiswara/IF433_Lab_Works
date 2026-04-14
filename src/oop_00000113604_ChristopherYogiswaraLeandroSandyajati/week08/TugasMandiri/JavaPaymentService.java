14342a67c4faa85f0d517158d328622d1fd44b9e
CYogiswara <112369336+CYogiswara@users.noreply.github.com>
2026-04-14 16:02:58 +0700

    week08: (task) create JavaPaymentService for payment interop

---
+++ src/oop_00000113604_ChristopherYogiswaraLeandroSandyajati/week08/TugasMandiri/JavaPaymentService.java
@@ -0,0 +1,6 @@
+package oop_00000113604_ChristopherYogiswaraLeandroSandyajati.week08.TugasMandiri;
+
+public class JavaPaymentService {
+    public static String processPayment(String productId){
+        return "TRX-" + productId + "-SUCCESS";}
+}
