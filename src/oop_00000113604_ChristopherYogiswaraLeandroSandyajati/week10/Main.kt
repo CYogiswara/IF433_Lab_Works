4d85f80470dd16ee8d952e47b9e569a89d595aa1
CYogiswara <112369336+CYogiswara@users.noreply.github.com>
2026-04-28 09:32:51 +0700

    week10: test math operations and comparable constraints

--- src/oop_00000113604_ChristopherYogiswaraLeandroSandyajati/week10/Main.kt
+++ src/oop_00000113604_ChristopherYogiswaraLeandroSandyajati/week10/Main.kt
@@ -1,20 +1,25 @@
 package oop_00000113604_ChristopherYogiswaraLeandroSandyajati.week10
 
 fun main(){
-    println("=== TEST GENERIC CLASS ===")
+    println("\n=== TEST GENERIC CLASS ===")
     val intBox = box(100)
     val stringbox = box("Generics in kotlin")
 
     println("Isi intbox: ${intBox.value}")
     println("Isi stringbox: ${stringbox.value}")
 
-    println("=== TEST MULTIPLE VARIABLES ===")
+    println("\n=== TEST MULTIPLE VARIABLES ===")
 
     val itemPrice = PairBox("Bitcoin", 65000)
     println("asset: ${itemPrice.key}, harga: ${itemPrice.value} USD")
 
-    println("=== TEST GENETIC FUNCTION ===")
+    println("\n=== TEST GENETIC FUNCTION ===")
     printData(3.14)
     val results = processData("Stable coin")
     println("Hasil proses: $results")
+
+    println("\n=== TEST CONSTRAINTS ===")
+    val math = Mathbox(10.5,20)
+    println("Total: ${math.sum()}")
+    println("Terbesar: ${getMax(45,90)}")
 }
