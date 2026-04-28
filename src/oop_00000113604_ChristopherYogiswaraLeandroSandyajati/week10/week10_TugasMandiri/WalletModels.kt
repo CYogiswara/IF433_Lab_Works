4a3d05c0d46ebeb192819b59a0f41ab824a99841
CYogiswara <112369336+CYogiswara@users.noreply.github.com>
2026-04-28 09:35:44 +0700

    week10: (task) create Coin and Transaction models

---
+++ src/oop_00000113604_ChristopherYogiswaraLeandroSandyajati/week10/week10_TugasMandiri/WalletModels.kt
@@ -0,0 +1,4 @@
+package oop_00000113604_ChristopherYogiswaraLeandroSandyajati.week10.week10_TugasMandiri
+
+data class Coin(val name: String, val balance: Double)
+data class Transaction(val id: Int, val amount: Double)
