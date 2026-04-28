b280dba3a73ea72b8491da89d90683c7fa412004
CYogiswara <112369336+CYogiswara@users.noreply.github.com>
2026-04-28 09:43:23 +0700

    week10: (task) implement generic WalletRepository

---
+++ src/oop_00000113604_ChristopherYogiswaraLeandroSandyajati/week10/week10_TugasMandiri/WalletRepository.kt
@@ -0,0 +1,12 @@
+package oop_00000113604_ChristopherYogiswaraLeandroSandyajati.week10.week10_TugasMandiri
+
+class WalletRepository<T>(){
+    private val items =  mutableListOf<T>()
+
+    fun add(item: T){
+        items.add(item)
+    }
+    fun getAll(): List<T>{
+        return items
+    }
+}
