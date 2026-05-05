e0eb96e1211f9cf4ae17fd7210c24fb234ddf343
CYogiswara <112369336+CYogiswara@users.noreply.github.com>
2026-05-05 13:06:05 +0700

    week11: create string extension functions

---
+++ src/oop_00000113604_ChristopherYogiswaraLeandroSandyajati/week11/StringExtension.kt
@@ -0,0 +1,9 @@
+package oop_00000113604_ChristopherYogiswaraLeandroSandyajati.week11
+
+fun String.addGreeting(): String{
+    return "hello, $this"
+}
+
+fun String.repeatTimes(n: Int): String{
+    return this.repeat(n)
+}
