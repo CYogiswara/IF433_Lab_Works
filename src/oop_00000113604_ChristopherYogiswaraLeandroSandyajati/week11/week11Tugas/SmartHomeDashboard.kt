8644e606f9fbddc42ff1f45b42d64b356aad5412
CYogiswara <112369336+CYogiswara@users.noreply.github.com>
2026-05-05 14:22:44 +0700

    week11: (task) configure Ezviz camera with also

--- src/oop_00000113604_ChristopherYogiswaraLeandroSandyajati/week11/week11Tugas/SmartHomeDashboard.kt
+++ src/oop_00000113604_ChristopherYogiswaraLeandroSandyajati/week11/week11Tugas/SmartHomeDashboard.kt
@@ -3,12 +3,13 @@ package oop_00000113604_ChristopherYogiswaraLeandroSandyajati.week11.week11Tugas
 fun main(){
     val homeDevices = mutableListOf<SmartDevice>()
 
-    val device = SmartDevice().apply {
+    val device = SmartDevice("Ezviz outdoor", "Camera").apply {
         name = "Philips WiZ Living Room"
         category = "Lighting"
         isOnline = true
-        powerLoad = 12
+        powerLoad = 5
     }.also {
+        println("(LOG) Kamera terhubung")
         homeDevices.add(it)
     }
 }
