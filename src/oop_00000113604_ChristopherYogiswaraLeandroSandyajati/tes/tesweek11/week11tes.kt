a2da1ebe364c3bc6aab6d623c41bf094061727f6
CYogiswara <112369336+CYogiswara@users.noreply.github.com>
2026-05-05 13:22:51 +0700

    week11: implement let function for null safety

--- src/oop_00000113604_ChristopherYogiswaraLeandroSandyajati/tes/tesweek11/week11tes.kt
+++ src/oop_00000113604_ChristopherYogiswaraLeandroSandyajati/tes/tesweek11/week11tes.kt
@@ -4,6 +4,21 @@ fun String.dasarExtention(): String{
     return "Halo, $this"
 }
 
+fun String.RubahHurufBesarDepan(): String{
+   var hasil = ""
+   hasil = this.split("").joinToString(""){
+       it.replaceFirstChar{
+           c -> c.uppercase()
+       }
+   }
+    return hasil
+}
+
 fun main(){
     println("Yogi".dasarExtention())
+
+    val nama1 = "Budi Sihombing"
+    println(nama1.uppercase())
+
+    println("Budi udin".RubahHurufBesarDepan())
 }
