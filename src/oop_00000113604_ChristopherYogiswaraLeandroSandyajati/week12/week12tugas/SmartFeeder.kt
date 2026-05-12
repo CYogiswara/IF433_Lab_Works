30e9586c5c78528fb7d8590d39eb1b4177b315c9
CYogiswara <112369336+CYogiswara@users.noreply.github.com>
2026-05-12 14:14:36 +0700

    week12: (task) implement food stock validation with throw

--- src/oop_00000113604_ChristopherYogiswaraLeandroSandyajati/week12/week12tugas/SmartFeeder.kt
+++ src/oop_00000113604_ChristopherYogiswaraLeandroSandyajati/week12/week12tugas/SmartFeeder.kt
@@ -7,5 +7,9 @@ fun dispenseKibble(requestedGram: Int, availableGram: Int, isJammed: Boolean): I
     if(isJammed){
         throw DispenserJamException()
     }
+
+    if(requestedGram > availableGram){
+        throw FoodEmptyException(requestedGram, requestedGram)
+    }
     return availableGram - requestedGram
 }
